0c2874ed8d4e20c0f634aa34b38f2d370c70bfc3
sandyrocky <119575532+sandyrocky@users.noreply.github.com>
2022-12-26 08:04:07 -0800

    Add files via upload

---
+++ Q-2.py
@@ -0,0 +1,10 @@
+'''Write a Python program to get the Python version you are using'''
+
+# first import sys -system .
+import sys
+
+#print() and write  inside the parenthesis.it tell you what type of version you are using.
+print(sys.version)
+
+#print () it give the version information.
+print(sys.version_info)
